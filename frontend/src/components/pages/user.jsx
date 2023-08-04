94d6344baf1284c4e436c6180e6036717781fe6f
Emi <emiliano.castro@mi.unc.edu.ar>
2023-08-04 05:33:03 -0300

    featureCerrarSesion

--- frontend/src/components/pages/user.jsx
+++ frontend/src/components/pages/user.jsx
@@ -1,10 +1,16 @@
-
-
+import {Link} from 'react-router-dom'
+import {useContext} from 'react'
+import {dataContext} from '../dataContext'
 const User = () => {
-
+  const {setUserData} = useContext(dataContext)
+  const handleClick = (e) => {
+    e.preventDefault();
+    setUserData(null)
+    window.location.href="/register"
+  }
   return( 
 <>
-    
+    <Link onClick={handleClick}>Cerrar Sesion</Link>
 </>
   )
 }
