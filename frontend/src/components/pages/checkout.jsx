3a84bb1636c8f860cc93a4d865d5d47fb032f024
Emi <emiliano.castro@mi.unc.edu.ar>
2023-08-05 19:39:13 -0300

    bugFixCheckout

--- frontend/src/components/pages/checkout.jsx
+++ frontend/src/components/pages/checkout.jsx
@@ -41,8 +41,7 @@ const Checkout = () => {
         {cart.map((item) => (
           <li key={item.id} className="checkout-item">
             <span className="item-name">{item.name}</span>
-            <span className="item-quantity">Cantidad: {item.quantity}</span>
-            <span className="item-price">Precio unitario: ${item.price}</span>
+            <span className="item-price">$ {item.price}</span>
           </li>
         ))}
       </ul>
